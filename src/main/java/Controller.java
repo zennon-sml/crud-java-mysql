91ecd4bb1bb093e86b12ad0cba037ec70c10008d
zennon-sml <zennonsampaioml@gmail.com>
2022-06-28 13:02:16 -0300

    select all de todos

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,18 +1,32 @@
 import escolha.Escolha;
 import java.util.Scanner;
 public class Controller {
+//    public static void main(String[] args){
+//        //Aluno aluno = new Aluno();
+//        //Views.printarEntidade(Models.selectAll("endereco"));
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("1-Aluno\n2-Professor\nR:");
+//        int resposta = input.nextInt();
+//        Escolha esc = new Escolha();
+//        if (resposta == 1){
+//            esc.aluno();
+//        } else if (resposta == 2) {
+//            esc.professor();
+//        }
+//    }
     public static void main(String[] args){
-        //Aluno aluno = new Aluno();
-        //Views.printarEntidade(Models.selectAll("endereco"));
+        //Models.customQuery("INSERT into endereco (rua,numero,cidade,cep,estado) VALUES (?,?,?,?)",);
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("1-Aluno\n2-Professor\nR:");
-        int resposta = input.nextInt();
-        Escolha esc = new Escolha();
-        if (resposta == 1){
-            esc.aluno();
-        } else if (resposta == 2) {
-            esc.professor();
-        }
+        System.out.println("Professores:");
+        Models.selectAllProfessor();
+        System.out.println("Alunos:");
+        Models.selectAllAluno();
+        System.out.println("Enderecos:");
+        Models.selectAllEndereco();
+        System.out.println("Disciplinas:");
+        Models.selectAllDisciplina();
+        System.out.println("Cursos:");
+        Models.selectAllCurso();
     }
 }
--- src/main/java/Models.java
+++ src/main/java/Models.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Date;
 
 public class Models {
     public static Connection fazerConexao() {
@@ -18,17 +19,143 @@ public class Models {
             return null;
         }
     }
-    public static ResultSet selectAll(String entidade){
+//    public static void customQuery(String query){
+//        try {
+//            Connection con = fazerConexao();
+//            PreparedStatement stmt = con.PreparedStatement();
+//            stmt.executeQuery(query);
+//            System.out.println("Query feita!!!!");
+//        }
+//        catch (Exception e) {
+//            System.out.println(e);
+//
+//        }
+//    }
+
+    public static void selectAllEndereco(){
         try {
             Connection con = fazerConexao();
             Statement stmt = con.createStatement();
-            String query = "select * from "+entidade;
+            String query = "select * from endereco;";
             ResultSet rs = stmt.executeQuery(query);
-            return rs;
+            while (rs.next()) {
+                //TODO instanciar objeto
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int id = rs.getInt(1);
+                String rua = rs.getString(2);
+                String numero = rs.getString(3);
+                String cidade = rs.getString(4);
+                String cep = rs.getString(5);
+                String estado = rs.getString(6);
+                System.out.println(id + "-" + rua + "-" + numero + "-" + cidade + "-" + cep + "-" + estado);
+                //TODO fazer um objeto e mandar pras views pra elas printar
+            }
         }
         catch (Exception e) {
             System.out.println(e);
-            return null;
+
+        }
+    }
+    public static void selectAllAluno(){
+        try {
+            Connection con = fazerConexao();
+            Statement stmt = con.createStatement();
+            String query = "select * from aluno;";
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                //TODO instanciar objeto
+                int id = rs.getInt(1);
+                String codigo = rs.getString(2);
+                String nome = rs.getString(3);
+                String telefone = rs.getString(4);
+                Date nasc = rs.getDate(5);
+                String sexo = rs.getString(6);
+                int fk_endereco = rs.getInt(7);
+                System.out.println(id + "-" + codigo + "-" + nome + "-" + telefone + "-" + nasc + "-" + sexo + "-" + fk_endereco);
+                //TODO fazer um objeto e mandar pras views pra elas printar
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void selectAllProfessor(){
+        try {
+            Connection con = fazerConexao();
+            Statement stmt = con.createStatement();
+            String query = "select * from professor;";
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()){
+                int id = rs.getInt(1);
+                String registro = rs.getString(2);
+                String nome = rs.getString(3);
+                String telefone = rs.getString(4);
+                String especialidade = rs.getString(5);
+                String titulacao = rs.getString(6);
+                int fk_endereco = rs.getInt(7);
+                System.out.println(id + "-" + registro + "-" + nome + "-" + telefone + "-" + especialidade + "-" + titulacao);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public static void selectAllDisciplina(){
+        try {
+            Connection con = fazerConexao();
+            Statement stmt = con.createStatement();
+            String query = "select * from disciplina;";
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()){
+                int id = rs.getInt(1);
+                int codigo = rs.getInt(2);
+                String descricao = rs.getString(3);
+                String carga_horaria = rs.getString(4);
+                int num_credito = rs.getInt(5);
+                System.out.println(id + "-" + codigo + "-" + descricao + "-" + carga_horaria + "-" + num_credito);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public static void selectAllTurma(){
+        try {
+            Connection con = fazerConexao();
+            Statement stmt = con.createStatement();
+            String query = "select * from turma;";
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()){
+                int id = rs.getInt(1);
+                int codigo = rs.getInt(2);
+                String sala = rs.getString(3);
+                String horario = rs.getString(4);
+                int fk_professor_disciplina = rs.getInt(5);
+                System.out.println(id + "-" + codigo + "-" + sala + "-" + horario + "-" + fk_professor_disciplina);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public static void selectAllCurso(){
+        try {
+            Connection con = fazerConexao();
+            Statement stmt = con.createStatement();
+            String query = "select * from curso;";
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()){
+                int id = rs.getInt(1);
+                int codigo = rs.getInt(2);
+                String nome = rs.getString(3);
+                String descricao = rs.getString(4);
+                int fk_aluno = rs.getInt(5);
+                int fk_disciplina = rs.getInt(6);
+                System.out.println(id + "-" + codigo + "-" + nome + "-" + descricao + "-" + fk_aluno + "-" + fk_disciplina);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
         }
     }
 }
